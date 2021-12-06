810b5beb123a0b60c5def0c0ce6c68bf515ba9ed
Scott Chamberlain <sckott@fastmail.com>
2021-12-06 08:30:17 -0800

    docs build: install self

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,12 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import sys
 import os
 import shlex
-import pyror
 import sphinx_rtd_theme
+
+import pyror
 
 sys.path.insert(0, os.path.abspath(".."))
 
